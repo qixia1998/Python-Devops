c01bfc8d9d84dba9236d3ac768679d953fec4952
qixia1998 <qixia717138552@gmail.com>
2022-05-23 11:23:43 +0800

    Updated test

--- test.py
+++ test.py
@@ -2,17 +2,17 @@ from app import app
 
 
 def test1():
-    response = app.test_client.get('/')
+    response = app.test_client().get('/')
     assert response.status_code == 200
 
 
 def test2():
-    response = app.test_client.get('/edit')
+    response = app.test_client().get('/edit')
     assert response.status_code == 200
 
 
 def test3():
-    response = app.test_client.get('/edit')
+    response = app.test_client().get('/edit')
     assert b"To Do App" in response.data
     assert b"Todo Title" in response.data
     assert b"Add" in response.data
